88b541f35fd46547ba2981e1b36553d3dcf466d1
Seph <paulo.seph@hotmail.com>
2020-02-06 15:42:47 -0300

    att 1

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5;
+		System.out.println(a);
+
+		var b = 4.5;
+		System.out.println(b);
+		b = 4;
+
+//	var j = 4;
+//	j = 4.5;
+
+		double d;
+		d = 123.65;
+		System.out.println(d);
+
+	}
+}
---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+	byte anosDeEmpresa = 23;
+	short numeroDeVoos = 542;
+	int id = 56789;
+	long pontosAcumulados = 3_134_845_223L;
+	
+	float salario = 11_445.00F;
+	double vendas = 2_555_345_130.01;
+	
+}
